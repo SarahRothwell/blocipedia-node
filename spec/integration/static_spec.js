5911d5d95ebcd064254b8717ca293cb952a0bbc0
Sarah Rothwell <sarahhrothwell@gmail.com>
2019-05-15 10:17:26 -0400

    Scaffolding and deployment assignment commit

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,11 +5,12 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-
     it("should return status code 200", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Blocipedia");
+
         done();
       });
     });
